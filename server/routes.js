eb7e4cec2ddfbe8a5f028199d8591ccdbcc0556d
Klemen Kenda <klemen.kenda@ijs.si>
2015-10-30 05:58:24 +0100

    close-base added to routes.

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,9 @@
 function setup(app, handlers) {
     // root - http://localhost:XYZ/
     app.get('/', handlers.service.handleGetRouterPaths.bind(handlers.service));
+
+    // http://localhost:XYZ/close-base
+    app.get('/close-base', handlers.service.handleCloseBase.bind(handlers.service));    
 }
 
 exports.setup = setup;
