8038be9b6f141672d5db866b0c43946731ac9419
Klemen Kenda <klemen.kenda@ijs.si>
2015-10-30 05:50:41 +0100

    Copied basic structure from Mobis Traffic Expert.

--- seensy.js
+++ seensy.js
@@ -1,4 +1,15 @@
 ﻿var qm = require('qminer');
+var server = require('./server/server.js');
+var createBase = require('./schema/create.js');
+var env = process.env.NODE_ENV || 'development';
+var config = require('./config.json')[env];
 
-console.log('Hello world!!!');
+qm.verbosity(0);
 
+// read input script argument for mode type. Default is "cleanCreate"
+var scriptArgs = (process.argv[2] == null) ? "cleanCreate" : process.argv[2];
+var base = createBase.mode(scriptArgs);
+
+// start server
+server.init(base);
+server.start(config.dataService.server.port);
