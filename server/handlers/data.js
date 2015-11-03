c455b641d5711d515242d1699511ea08a14c813a
Rok Mocnik <rok.mocnik@gmail.com>
2015-11-03 14:39:38 +0100

    Added optional control to add-measurement and moved it outside of it's request function Added get-measurement

--- server/handlers/data.js
+++ server/handlers/data.js
@@ -20,11 +20,12 @@ DataHandler.prototype.setupRoutes = function (app) {
     app.get(this.namespace + 'get-current-aggregates', this.hangleGetCurrentAggregates.bind(this));
     // get-nodes - Get information on nodes and their sensors
     app.get(this.namespace + 'get-nodes', this.hangleGetNodes.bind(this));
+    // get-measurement - Get measurements from specified store during dates
+    app.get(this.namespace + 'get-measurement', this.hangleGetMeasurement.bind(this));
 }
 
 /**
- * Get data from JSON, save node, type and measurements to the stores.
- * If measurement store does not exist create it, together with all aggregates.
+ * Parse data from request and send it to add measurements
  *
  * @param req  {model:express~Request}  Request
  * @param res  {model:express~Response}  Response  
@@ -34,6 +35,22 @@ DataHandler.prototype.handleAddMeasurement = function (req, res) {
     logger.debug('[AddMeasurement] ' + req.query.data)
     var data = JSON.parse(req.query.data);
     
+    this.addMeasurement(data);
+
+    res.status(200).json({'done' : 'well'}).end();
+}
+
+/**
+ * Save node, type and measurements to the stores.
+ * If measurement store does not exist create it, together with all aggregates.
+ *
+ * @param data  {model:express~Request}  data to be added to stores
+ */
+DataHandler.prototype.addMeasurement = function (data, control){
+    // Check if we want to do control
+    control = typeof control !== 'undefined' ? control : true;
+
+    // Walk thru the data
     for (i = 0; i < data.length; i++) {
         // Parse and store node information
         var node = new Object();
@@ -41,9 +58,9 @@ DataHandler.prototype.handleAddMeasurement = function (req, res) {
         node.Position = new Array();
         node.Position[0] = data[i].node.lat;
         node.Position[1] = data[i].node.lng;
-
+        
         var nodeid = this.base.store("Node").push(node);
-
+        
         var measurements = data[i].node.measurements;
         for (j = 0; j < measurements.length; j++) {
             // Parse and store type information
@@ -51,22 +68,22 @@ DataHandler.prototype.handleAddMeasurement = function (req, res) {
             type.Name = measurements[j].type.name;
             type.Phenomena = measurements[j].type.phenomenon;
             type.UoM = measurements[j].type.UoM;
-
+            
             var typeid = this.base.store("Type").push(type);
-
+            
             // Parse and store node information
             var sensor = new Object();
             sensor.Name = measurements[j].sensorid;
             sensor.NodeId = nodeid;
             sensor.TypeId = typeid;
-
+            
             var sensorid = this.base.store("Sensor").push(sensor);
             
             // Create names for additional stores
             var measurementStoreStr = "M" + nameFriendly(sensor.Name);
             var aggregateStoreStr = "A" + nameFriendly(sensor.Name);
             var resampledStoreStr = "R" + nameFriendly(sensor.Name);
-
+            
             var measurementStore = this.base.store(measurementStoreStr);
             
             // If the store does not exist
@@ -84,67 +101,69 @@ DataHandler.prototype.handleAddMeasurement = function (req, res) {
                             { "field": "Date", "type": "value", "sort": "string", "vocabulary": "date_vocabulary" }
                         ]
                     }]);
-
-                // Create aggregate store names 'A-sensorname'
-                var aggregateStoreDefinition = this.getAggregateStoreStructure(aggregateStoreStr);
-                this.base.createStore([aggregateStoreDefinition]);
-
-                // Add a tick
-                measurementStore.addStreamAggr({
-                    name: "tick", type: "timeSeriesTick",
-                    timestamp: "Time", value: "Val"
-                })
-
-                // Adding tick based aggregates
-                tickTimes.forEach(function (time) {
-                    tickAggregates.forEach(function (aggregate) {
-                        aggregateObj = {
-                            name: aggregate.name + time.name, type: aggregate.type, inAggr: "tick",
-                            emaType: "previous", interval: time.interval * 60 * 60 * 1000 - 1, initWindow: 0 * 60 * 1000
-                        };
-                        measurementStore.addStreamAggr(aggregateObj);
-                    })
-                });
-
-                // Adding winbuff based aggregates
-                bufTimes.forEach(function (time) {
-                    var bufname = 'winbuff' + time.name;
-                    // adding timeserieswinbuff aggregate
+                if (control) {
+                    // Create aggregate store names 'A-sensorname'
+                    var aggregateStoreDefinition = this.getAggregateStoreStructure(aggregateStoreStr);
+                    this.base.createStore([aggregateStoreDefinition]);
+                    
+                    // Add a tick
                     measurementStore.addStreamAggr({
-                        name: bufname, type: "timeSeriesWinBuf",
-                        timestamp: "Time", value: "Val", winsize: time.interval * 60 * 60 * 1000 - 1
+                        name: "tick", type: "timeSeriesTick",
+                        timestamp: "Time", value: "Val"
+                    })
+                    
+                    // Adding tick based aggregates
+                    tickTimes.forEach(function (time) {
+                        tickAggregates.forEach(function (aggregate) {
+                            aggregateObj = {
+                                name: aggregate.name + time.name, type: aggregate.type, inAggr: "tick",
+                                emaType: "previous", interval: time.interval * 60 * 60 * 1000 - 1, initWindow: 0 * 60 * 1000
+                            };
+                            measurementStore.addStreamAggr(aggregateObj);
+                        })
                     });
                     
-                    bufAggregates.forEach(function (aggregate) {
-                        aggregateObj = {
-                            name: aggregate.name + time.name, type: aggregate.type, inAggr: bufname
-                        };
-                        measurementStore.addStreamAggr(aggregateObj);
-                    })
-                });
-
+                    // Adding winbuff based aggregates
+                    bufTimes.forEach(function (time) {
+                        var bufname = 'winbuff' + time.name;
+                        // adding timeserieswinbuff aggregate
+                        measurementStore.addStreamAggr({
+                            name: bufname, type: "timeSeriesWinBuf",
+                            timestamp: "Time", value: "Val", winsize: time.interval * 60 * 60 * 1000 - 1
+                        });
+                        
+                        bufAggregates.forEach(function (aggregate) {
+                            aggregateObj = {
+                                name: aggregate.name + time.name, type: aggregate.type, inAggr: bufname
+                            };
+                            measurementStore.addStreamAggr(aggregateObj);
+                        })
+                    });
+                }
+                
                 logger.debug('[Add Measurement] Created new measurement store')
                 logger.debug('[Add Measurement] Aggregates: ' + measurementStore.getStreamAggrNames())
             }
-
+            
             // Parse and store measurement
             var measurement = new Object();
             measurement.Val = Number(measurements[j].value);
             measurement.Time = measurements[j].timestamp;
             measurement.Date = measurements[j].timestamp.substr(0, 10);
-                        
-            measurementStore.push(measurement);
-
-            // Store current aggregates
-            var aggregateStore = this.base.store(aggregateStoreStr);
-            var aggregateid = aggregateStore.push(this.getCurrentAggregates(measurementStore));
             
+            // TODO: Check if measurement is new or not
+            measurementStore.push(measurement);
+            
+            // Store current aggregates
+            if (control) {
+                var aggregateStore = this.base.store(aggregateStoreStr);
+                var aggregateid = aggregateStore.push(this.getCurrentAggregates(measurementStore));
+            }
             logger.debug('[AddMeasurement] Pushed' + '{ "Val": ' + measurement.Val + ', "Time": "' + measurement.Time + '", "Date": "' + measurement.Date + '"}');
         }
     }
-
-    res.status(200).json({'done' : 'well'}).end();
 }
+
 
 /**
  * Get structure of specified aggregate store
@@ -305,6 +324,57 @@ DataHandler.prototype.hangleGetNodes = function (req, res) {
 }
 
 /**
+ *  Get measurements from specified store
+ *
+ * @param req  {model:express~Request}   Request
+ * @param res  {model:express~Response}  Response  
+ */
+DataHandler.prototype.hangleGetMeasurement = function (req, res) {
+    var sensorName = req.query.name;
+    var startDate = req.query.startdate;
+    var endDate = req.query.enddate;
+
+    var measurementStoreStr = "M" + nameFriendly(String(sensorName));
+
+    this.addDate(startDate, endDate);
+
+    var measuredRSet = this.base.search({
+        "$from": measurementStoreStr,
+        "Date": [{ "$gt": String(startDate) }, { "$lt": String(endDate) }]
+    });
+
+    if (measuredRSet == null) str = "[]";
+    else {
+        str = "[\n";
+        for (var i = 0; i < measuredRSet.length; i++) {
+            str += '  { "Val":' + measuredRSet[i].Val + ', "Timestamp": "' + measuredRSet[i].Time.toISOString().replace(/Z/, '') + '"}';
+            if (i != measuredRSet.length - 1) str += ',\n';
+        }
+        str += "\n]";
+    }
+    res.status(200).send(str);
+}
+
+/**
+ * Add dummy sensor measurement (to inser date into a common key vocabulary)
+ *
+ * @param startDateStr  {String}  Starting date
+ * @param enddateStr    {String}  End date
+ */
+DataHandler.prototype.addDate = function (startDateStr, endDateStr) {
+    var startDateRequest = '[{"node":{"id":"virtual-node","name":"virtual-node","lat":0,"lng":0, \
+        "measurements":[{"sensorid":"virtual-node-request","value":1.0,"timestamp":"' + startDateStr +
+        'T00:00:00.000","type":{"id":"0","name":"virtual-request","phenomenon":"request","UoM":"r"}}]}}]';
+    var endDateRequest = '[{"node":{"id":"virtual-node","name":"virtual-node","lat":0,"lng":0, \
+        "measurements":[{"sensorid":"virtual-node-request","value":1.0,"timestamp":"' + endDateStr +
+        'T00:00:00.000","type":{"id":"0","name":"virtual-request","phenomenon":"request","UoM":"r"}}]}}]';
+    logger.debug('[Add date]' + startDateRequest);
+    logger.debug('[Add date]' + endDateRequest);
+    this.addMeasurement(JSON.parse(startDateRequest), false);
+    this.addMeasurement(JSON.parse(endDateRequest), false);
+}
+
+/**
  * Modify string to alpha numeric
  *
  * @param myName  {String}  input
@@ -312,6 +382,8 @@ DataHandler.prototype.hangleGetNodes = function (req, res) {
  */
 function nameFriendly(myName) {
     return myName.replace(/\W/g, '');
-};
+}
+
+
 
 module.exports = DataHandler;
