15e8cc90c93cf0abf6205eecb6acd1a774fe2226
Rok Mocnik <rok.mocnik@gmail.com>
2015-11-17 11:15:40 +0100

    Revert "Work on backup solution"
    This reverts commit cb762bb0cadcd378227993cdc421065d62dc2d32.

--- schema/io.js
+++ schema/io.js
@@ -1,12 +1,10 @@
 var fs = require('qminer').fs;
-var fsnode = require('fs')
 var path = require('path');
 var logger = require('../modules/logger/logger.js');
 var createBase = require('./create.js');
 var env = process.env.NODE_ENV || 'development';
 var config = require('../config.json')[env];
 var ncp = require('ncp').ncp;
-var rimraf = require('rimraf')
 var mkdirp = require('mkdirp');
 require('../server/handlers/config.js');
 
@@ -129,21 +127,6 @@ function closeBase(base) {
     logger.info('[Main] Done!');
 }
 
-function getLatestBackup(dir){
-    function getDirectories(srcpath) {
-        return fsnode.readdirSync(srcpath).filter(function (file) {
-            return fsnode.statSync(path.join(srcpath, file)).isDirectory();
-        });
-    }
-    backups = getDirectories(dir);
-    var backup_id = 0;
-    for (var i = 0; i < backups.length; i++) {
-        var ii = parseInt(backups[i]);
-        if (ii > backup_id) backup_id = ii;
-    }
-    return backup_id;
-}
-
 /**
  * Open base
  *
@@ -153,39 +136,13 @@ function openBase(open) {
     if (open == 'backup') {
         open = 'open';
         // TODO: move files to db folder
-        
-        // Find latest backup
-        var num = getLatestBackup(path.join(__dirname, './backup/'))
-        // Delete current db create new folder and copy backup into it
-        db_folder = path.join(__dirname, './db/');
-        backup_folder = path.join(__dirname, './backup/' + num + '/');
-        logger.debug('[Main] ' + backup_folder);
-        logger.debug('[Main] ' + db_folder);
-        rimraf(db_folder, function (err) {
-            logger.debug('[Main] ' + err);
-            mkdirp(db_folder, function (err) {
-                logger.debug('[Main] ' + err);
-                ncp(backup_folder, db_folder, function (err) {
-                    logger.debug('[Main] ' + err);
-                    var base = createBase.mode(open);
-                    if (open == 'open') {
-                        loadStreamAggrs(base);
-                        logger.info('[Main] Loaded stream aggregates');
-                    }
-                    return base;
-                });
-            });
-        });
-
-    } else {
-        var base = createBase.mode(open);
-        if (open == 'open') {
-            loadStreamAggrs(base);
-            logger.info('[Main] Loaded stream aggregates');
-        }
-        return base;
     }
-    
+    var base = createBase.mode(open);
+    if (open == 'open') {
+        loadStreamAggrs(base);
+        logger.info('[Main] Loaded stream aggregates');
+    }
+    return base;
 }
 
 /**
@@ -222,8 +179,7 @@ function backup(base, server) {
         logger.info('[Main] Saved stream aggregates');
         closeBase(base);
         // Backup db files to another location
-        num = getLatestBackup(path.join(__dirname, './backup/'))
-        dest = path.join(__dirname, './backup/' + (num + 1) + '/');
+        dest = path.join(__dirname, './backup/' + new Date().toISOString().replace(/Z/, '').replace(/:/g, '').split('.')[0] + '/');
         source = path.join(__dirname, './db/');
         logger.debug(source);
         logger.debug(dest);
